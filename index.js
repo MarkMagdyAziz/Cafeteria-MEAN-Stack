433adde048446eb81d29eb368aef05fc88a94fc0
Mark M Aziz <45792943+MarkMagdyAziz@users.noreply.github.com>
2022-10-10 22:11:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,13 @@ const DB_URL = process.env.PORT || 'mongodb://localhost:27017/blogsApp'
 const PORT = process.env.PORT || 5000
 
 const authMiddleware = require('./middlewares/auth')
-const { userRouter } = require('./routes/users')
-const { postsRouter } = require('./routes/posts')
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded())
 
-// Morgan Middlwware Package
-// express.static middleware
+
 app.use(express.static('./public'))
 
 
@@ -24,20 +21,8 @@ app.use(express.static('./public'))
 //     next()
 // })
 
-// Check If Logged In
-// app.use('./users', [authMiddleware, todosRouter] => {
-
-// })
 
 
-// index.hbs
-// app.use('/', (req, res) => {
-//     const user = {
-//         name: '',
-//         email: '',
-//     }
-//     res.render('index', user)
-// })
 
 // // Handle Error Middleware
 // app.use((err, req, res, next) => {
@@ -45,24 +30,9 @@ app.use(express.static('./public'))
 //     res.status(500).end(err.message)
 // })
 
-// app.get((req, res, next) => {
-//     console.log(`${new Date()} - ${req.method} - ${req.url}`) //server
-//     next()
-// })
 
 
-// app.use((req, res, next) => {
-//     console.log(`${new Date()} - ${req.method} - ${req.url}`) //server
-//     next()
-// })
 
-app.use(['/users', '/user'], userRouter)
-app.use(['/posts', '/post'], postsRouter)
-// app.use(['/posts', '/post'], authMiddleware)
-
-app.get('/', (req, res) => {
-
-})
 
 // logg middle ware
 app.use((req, res, next) => {
@@ -70,7 +40,6 @@ app.use((req, res, next) => {
     next()
 })
 
-// app.use('/users', authMiddleware)
 mongoose.connect(DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
