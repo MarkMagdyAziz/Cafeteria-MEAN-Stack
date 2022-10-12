aef88012d54ae2917b4b17e4bc1019601b513c3c
IbrahimOmar8 <IbrahimJPM8@gmail.com>
2022-10-12 17:22:48 +0200

    Edite cat and product

--- models/product.js
+++ models/product.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const validate = require("mongoose-validator");
+//const validate = require("mongoose-validator");
 
 const prodctSchema = new mongoose.Schema({
   name: {
